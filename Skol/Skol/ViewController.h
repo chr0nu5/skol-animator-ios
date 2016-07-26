4f2bdd1140d1cecc6a5ad3d7b2ea296184e05bf3
Elliot <robot@Elliot-Alderson.local>
2016-07-25 23:32:43 -0300

    Add Myo Framework

---
+++ Skol/Skol/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Skol
+//
+//  Created by Elliot on 7/25/16.
+//  Copyright © 2016 FSociety. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Skol/Skol/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  Skol
+//
+//  Created by Elliot on 7/25/16.
+//  Copyright © 2016 FSociety. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithRed:255/255.0 green:209/255.0 blue:10/255.0 alpha:1];
+    
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
